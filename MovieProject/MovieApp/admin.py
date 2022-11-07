77004e8cbd01b71786322273abce5ebad8adca50
venkatchouhan <venkatchouhan01@gmail.com>
2022-11-07 14:49:41 +0530

    firstcommit

---
+++ MovieProject/MovieApp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from MovieApp.models import Movies
+
+#Register your models here.
+class MoviesAdmin(admin.ModelAdmin):
+    list_display=['releasedate','moviename', 'actor','actress','rating'];
+
+admin.site.register(Movies,MoviesAdmin);
